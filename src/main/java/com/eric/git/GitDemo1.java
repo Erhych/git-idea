0ea7b47cf42a7a55f788faf840fd947d1e75e460
eric <645175997@qq.com>
2022-08-16 20:13:43 +0800

    first

---
+++ src/main/java/com/eric/git/GitDemo1.java
@@ -0,0 +1,12 @@
+package com.eric.git;
+
+/**
+ * @author Eric Jiang
+ * @Date 2022-08-16-8:12 下午
+ * @Description
+ */
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hellogit 01");
+    }
+}
